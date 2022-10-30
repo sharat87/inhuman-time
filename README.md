1a1633e000c2f1009b05ebeb43575c48396eba5c
Shrikant Sharat Kandula <shrikantsharat.k@gmail.com>
2022-10-30 19:14:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Inhuman Time
+
+This extension changes relative time displays like `3 days ago`, `20 minutes ago` etc. to display the actual full date
+and time.
+
+![Preview](https://raw.githubusercontent.com/sharat87/inhuman-time/master/media/preview.png)
+
+## [Install on Firefox](https://addons.mozilla.org/en-US/firefox/addon/inhuman-time/)
+
+## [Install on Chrome](https://chrome.google.com/webstore/detail/inhuman-time/nhmgmejfdbadfenlggckjdjamnhngbib)
+
+
---
+++ content_script.js
@@ -0,0 +1,72 @@
+const timeFormat = new Intl.RelativeTimeFormat("en", {
+	localeMatcher: "best fit",
+	numeric: "auto",
+})
+
+const mutationObserver = new MutationObserver(applyWrapped);
+
+applyWrapped()
+
+function applyWrapped() {
+	try {
+		apply()
+	} catch (e) {
+		console.error("Error loading inhuman-time", e)
+	}
+}
+
+function apply() {
+	mutationObserver.disconnect()
+
+	for (const el of document.querySelectorAll("relative-time")) {
+		const actualTime = new Date(el.getAttribute("datetime"))
+		const span = document.createElement("span")
+		span.innerText = el.getAttribute("title") || actualTime.toLocaleString()
+		span.title = getRelativeTime(actualTime)
+		span.style.textDecoration = "underline #888 dashed"
+		el.replaceWith(span)
+	}
+
+	mutationObserver.takeRecords()
+	mutationObserver.observe(document.body, {
+		childList: true,
+		subtree: true,
+	})
+}
+
+function getRelativeTime(t) {
+	let count = (Date.now() - t) / 1000
+
+	if (count < 60) {
+		return timeFormat.format(Math.round(count), "second")
+	}
+	count /= 60
+
+	if (count < 60) {
+		return timeFormat.format(Math.round(count), "minute")
+	}
+	count /= 60
+
+	if (count < 24) {
+		return timeFormat.format(Math.round(count), "hour")
+	}
+	count /= 24
+
+	if (count < 7) {
+		return timeFormat.format(Math.round(count), "day")
+	}
+	count /= 7
+
+	if (count < 4) {
+		return timeFormat.format(Math.round(count), "week")
+	}
+	count /= 4
+
+	if (count < 12) {
+		return timeFormat.format(Math.round(count), "month")
+	}
+	count /= 12
+
+	return timeFormat.format(Math.round(count), "year")
+}
+
